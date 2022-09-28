3913839a2003841566761e64379edf06ef859894
Rupam Das <rejisterjack@gmail.com>
2022-09-28 08:32:34 +0530

    changed router name

--- route/data.js
+++ route/data.js
@@ -2,5 +2,5 @@ const express = require("express");
 const blogController = require("../details/data");
 
 const blogRouter = express.Router();
-blogRouter.route("/brouter").get(blogController.apiController);
+blogRouter.route("/bdata").get(blogController.apiController);
 module.exports = blogRouter;
