357fb6d6f8a7d7c0af706aa84f148c1d64eee38c
Rupam Das <rejisterjack@gmail.com>
2022-09-28 08:50:05 +0530

    added default route

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(cors())
 app.use("/api",blogRouter)
 
+app.get('/',(req,res)=>{
+    res.send(`<h1>initial page of react blog backend server</h1>`)
+})
+
 
 app.listen(port,()=>{
     console.log(`server is running on port ${port}`);
